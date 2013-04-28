c470f50430437504e194efcb55d7ce92b9083eae
Sergei Myts <sergei.myts@gmail.com>
2013-04-28 23:37:21 +0300

    HMM implementation of input reading

---
+++ hmm.cc
@@ -0,0 +1,91 @@
+#include <vector>
+#include <iostream>
+
+#include "hmm.h"
+
+using HMM::Data::Model;
+using HMM::Data::ExperimentData;
+
+namespace
+{
+    /**
+     * \brief Converts first string element to char emission symbol for HMM
+     *
+     * \note
+     * String assumed to be non-empty and symbol[0] is supposed to be in a..z ascii.
+     */
+    char symbolToInd(const std::string& symbol)
+    {
+        return symbol[0] - 'a';
+    }
+};
+
+void Model::ReadModel(std::istream& modelSource)
+{
+    // part: states reading
+    size_t nstates;
+    std::string stateName;
+
+    modelSource >> nstates;
+
+    for (size_t i = 0; i < nstates; ++i) {
+        modelSource >> stateName;
+        stateNameToIndex[stateName] = i;
+    }
+
+    // part: alphabet reading
+    modelSource >> alphabetSize;
+
+    // part: transitions reading
+    size_t ntransitions;
+    std::string targetStateName;
+
+    transitionProb.assign(nstates, std::vector<double> (nstates, 0));
+    modelSource >> ntransitions;
+
+    for (size_t i = 0; i < ntransitions; ++i) {
+        double prob;
+        modelSource >> stateName >> targetStateName >> prob;
+
+        size_t fromInd = stateNameToIndex[stateName];
+        size_t toInd   = stateNameToIndex[targetStateName];
+
+        transitionProb[fromInd][toInd] = prob;
+    }
+
+    // part: state-symbol emission probabilities reading
+    size_t nemissions;
+    std::string symbol; // supposed to be single character, string is used for simpler reading code
+
+    stateSymbolProb.assign(nstates, std::vector<double> (alphabetSize, 0));
+    modelSource >> nemissions;
+
+    for (size_t i = 0; i < nemissions; ++i) {
+        double prob;
+        modelSource >> stateName >> symbol >> prob;
+
+        size_t stateInd = stateNameToIndex[stateName];
+        size_t symbolInd = symbolToInd(symbol);
+
+        stateSymbolProb[stateInd][symbolInd] = prob;
+    }
+}
+
+void ExperimentData::ReadExperimentData(const Model& model, std::istream& dataSource)
+{
+    size_t nsteps;
+    size_t stepNumber;
+    std::string stateName;
+    std::string symbol;// supposed to be single character, string is used for simpler reading code
+
+    dataSource >> nsteps;
+
+    for (size_t i = 0; i < nsteps; ++i) {
+        dataSource >> stepNumber >> stateName >> symbol;
+
+        size_t stateInd = model.stateNameToIndex.at(stateName);
+        size_t symbolInd = symbolToInd(symbol);
+
+        timeStateSymbol.emplace_back(stepNumber, stateInd, symbolInd);
+    }
+}
